6c1efdcf5da65656c828c4737a7f0a49b0d2d3d6
Obertan <80553319+ObertanBarcellos@users.noreply.github.com>
2022-07-07 04:00:31 -0300

    Next step is the slider

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Announcement from './components/Announcement';
+import Navbar from './components/Navbar';
+
+
+const App = () => {
+  return (
+    <div className="App">
+        <Announcement />
+        <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import styled from 'styled-components';
+import { BsSearch } from 'react-icons/bs';
+import Badge from '@mui/material/Badge';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+
+const Container = styled.div`
+    height: 60px;
+`;
+
+const Wrapper = styled.div`
+    padding: 10px 20px;
+    display: flex;
+    justify-content: space-between;
+`;
+
+const Left = styled.div`
+    flex: 1;
+    display: flex;
+    align-items: center;
+`;
+const Language = styled.span`
+    font-size: 14px;
+    cursor: pointer;
+`;
+const SearchContainer = styled.div`
+    border: 0.1px solid lightgrey;
+    display: flex;
+    align-items: center;
+    margin-left: 25px;
+    padding: 5px;
+`;
+const Input = styled.input`
+    border: none;
+    outline: none;
+`;
+
+
+
+const Center = styled.div`
+    flex: 1;
+    text-align: center;
+`;
+const Logo = styled.h1`
+    font-weight: bold;
+`;
+
+
+
+const Right = styled.div`
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+`;
+const MenuItem = styled.div`
+    font-size: 16px;
+    cursor: pointer;
+    margin-left: 15px;
+    margin-right: 15px;
+`;
+
+
+const Navbar = () => {
+  return (
+    <Container>
+       <Wrapper>
+            <Left>
+                <Language>EN</Language>
+                <SearchContainer>
+                    <Input />
+                    <SearchOutlinedIcon style={{color: 'gray', fontSize:16}}/>
+                </SearchContainer>
+            </Left>
+            <Center>
+                <Logo>NCL Sell</Logo>
+            </Center>
+            <Right>
+                <MenuItem>Register</MenuItem>
+                <MenuItem>Sign In</MenuItem>
+                <MenuItem>
+                <Badge badgeContent={4} color="primary">
+                    <ShoppingCartOutlinedIcon />
+                </Badge>
+                </MenuItem>
+            </Right>
+       </Wrapper>
+    </Container>
+  )
+}
+
+export default Navbar;
